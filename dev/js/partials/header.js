59ee9612a43ffb6edbd55df51db08350fffc0814
Patrick Petrushonis <pwp904@gmail.com>
2017-01-21 11:37:47 -0500

    Added mobile support to header nav.

--- dev/js/partials/header.js
+++ dev/js/partials/header.js
@@ -7,3 +7,17 @@ $(window).scroll(function() {
       $('.header-container').removeClass('fixed');
     }
 });
+
+// Expand header navigation mobile variant
+$('.header-controls__nav-toggle').click(function() {
+  $('.nav-menu').toggleClass('show-nav');
+
+  var headerNav = $('.header-controls ul');
+
+  if(headerNav.hasClass('show-nav')) {
+    headerNav.removeClass('show-nav');
+  }
+  else {
+    headerNav.addClass('show-nav');
+  }
+});
--- dev/js/partials/scroll.js
+++ dev/js/partials/scroll.js
@@ -2,11 +2,17 @@
 $('.header-controls ul li a').click(function(event) {
     var scrollOffset = 77;
     var scrollTargetId = $(this).attr("href");
-    var scrollTarget = $(id).offset().top - offset;
+    var scrollTarget = $(scrollTargetId).offset().top - scrollOffset;
 
     $('html, body').animate({
-        scrollTop: target
+        scrollTop: scrollTarget
     }, 500);
 
     event.preventDefault();
+
+	// Ensure mobile nav collapses on navigation scroll
+  	if($('.header-controls__nav-toggle input[type="checkbox"]').is(':checked')) {
+  		$('.header-controls__nav-toggle input[type="checkbox"]').prop('checked', false);
+  	}
+   	$('.header-controls ul').removeClass('show-nav');
 });
