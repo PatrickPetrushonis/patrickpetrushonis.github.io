486926816aebbe16d507aeb9e92d5460d27954b1
Patrick Petrushonis <pwp904@gmail.com>
2017-01-22 13:49:12 -0500

    Added scroll to page top in sticky header.

--- dev/js/partials/scroll.js
+++ dev/js/partials/scroll.js
@@ -1,5 +1,5 @@
 // Scroll to section from main navigation
-$('.header-controls ul li a').click(function(event) {
+$('.header-controls a').click(function(event) {
     var scrollOffset = 77;
     var scrollTargetId = $(this).attr("href");
     var scrollTarget = $(scrollTargetId).offset().top - scrollOffset;
@@ -11,8 +11,8 @@ $('.header-controls ul li a').click(function(event) {
     event.preventDefault();
 
 	// Ensure mobile nav collapses on navigation scroll
-  	if($('.header-controls__nav-toggle input[type="checkbox"]').is(':checked')) {
-  		$('.header-controls__nav-toggle input[type="checkbox"]').prop('checked', false);
-  	}
-   	$('.header-controls ul').removeClass('show-nav');
+	if($('.header-controls__nav-toggle input[type="checkbox"]').is(':checked')) {
+		$('.header-controls__nav-toggle input[type="checkbox"]').prop('checked', false);
+    $('.header-controls ul').removeClass('show-nav');
+	}
 });
