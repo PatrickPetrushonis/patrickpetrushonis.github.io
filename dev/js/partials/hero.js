4fcf1c4551df0955875d89c18af10b0bf145c124
Patrick Petrushonis <pwp904@gmail.com>
2017-01-30 20:59:29 -0500

    Added parallax stars to homepage hero.

--- dev/js/partials/hero.js
+++ dev/js/partials/hero.js
@@ -2,8 +2,8 @@ var patternColors = ['#2b275a', '#5d2659', '#1e314a', '#265479', '#387cb3'];
 
 function generatePattern() {
   var pattern = Trianglify({ 
-    width: window.innerWidth, 
-    height: window.innerHeight,
+    width: $(window).outerWidth(), 
+    height: $(window).outerHeight(),
     cell_size: 45,
     variance: 0.9,
     x_colors: patternColors
@@ -12,10 +12,12 @@ function generatePattern() {
   return pattern;
 }
 
-$(document).on('resize', function() {
-  $(document).find('.hero__container').prepend(generatePattern().canvas());
-});
+function handlePattern() {
+  var patternCanvas = generatePattern().canvas();
+  $(document).find('.hero__container').append(patternCanvas);
+  $(patternCanvas).addClass('hero__trianglify-canvas');
+}
 
-$(document).ready(function() {
-  $(document).find('.hero__container').prepend(generatePattern().canvas())
-});
+$(document).on('resize', function() { handlePattern(); });
+
+$(document).ready(function() { handlePattern(); });
