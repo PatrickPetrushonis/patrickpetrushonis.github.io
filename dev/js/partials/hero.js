ec3c10299b9bb644397e31acd935dc95d20bbb7f
Patrick Petrushonis <pwp904@gmail.com>
2017-08-13 14:10:54 -0400

    Improved mobile responsiveness of hero background.

--- dev/js/partials/hero.js
+++ dev/js/partials/hero.js
@@ -1,12 +1,15 @@
 var patternColors = ['#2b275a', '#5d2659', '#1e314a', '#265479', '#387cb3'];
 
 function generatePattern() {
+  var container = $(document).find('[container="hero"]');
+  var height = $(window).outerWidth() <= 768 ? container.outerHeight() : $(window).outerHeight() * 0.5;
+
   var pattern = Trianglify({ 
-    width: $(window).outerWidth(), 
-    height: $(window).outerHeight() * 0.5,
-    cell_size: 45,
-    variance: 0.9,
-    x_colors: patternColors
+      width: $(window).outerWidth(), 
+      height: height,
+      cell_size: 45,
+      variance: 0.9,
+      x_colors: patternColors
   });
 
   return pattern;
@@ -18,7 +21,7 @@ function handlePattern() {
 
   // Create a new canvas for hero container
   var patternCanvas = generatePattern().canvas();
-  $(document).find('.hero__container').append(patternCanvas);
+  $(document).find('[container="hero"]').append(patternCanvas);
   $(patternCanvas).addClass('hero__trianglify-canvas');
 }
 
