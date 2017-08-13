d0cb37dcdf4d58f81425e44d5824a1c47ffe32fb
Patrick Petrushonis <pwp904@gmail.com>
2017-08-13 16:31:48 -0400

    Decreased hero height on non-mobile screen sizes.

--- dev/js/partials/hero.js
+++ dev/js/partials/hero.js
@@ -4,9 +4,9 @@ function generatePattern() {
   var containerHeight = $(document).find('[container="hero"]').outerHeight();
   var windowHeight = $(window).outerHeight();
   var windowWidth = $(window).outerWidth();
-
+  
   if(windowHeight < containerHeight || windowWidth > 768) {
-    windowHeight = windowWidth <= 768 ? containerHeight : windowHeight * 0.5;
+    windowHeight = containerHeight;
   }
 
   var pattern = Trianglify({ 
