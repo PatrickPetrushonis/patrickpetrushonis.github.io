49a91da8af741a0f9d66d50bdb14304375151d9b
Patrick Petrushonis <pwp904@gmail.com>
2017-08-15 23:51:19 -0400

    Added offset on homepage section load.

--- dev/js/partials/scroll.js
+++ dev/js/partials/scroll.js
@@ -15,4 +15,26 @@ $('.header-controls a').click(function(event) {
 
     // Force clicked anchor to lose focus
     $(this).parent().blur();
+
+    // Ensure mobile nav collapses on navigation scroll
+    if($('.header-controls__nav-toggle input[type="checkbox"]').is(':checked')) {
+        $('.header-controls__nav-toggle input[type="checkbox"]').prop('checked', false);
+        $('.header-controls ul').removeClass('show-nav');
+    }
+});
+
+// Scroll to url section with header offset
+$(document).ready(function() {
+    var path = window.location.href;
+    var pathSub = path.split('#')[1];
+
+    if(pathSub !== undefined && pathSub !== path) {
+        var scrollOffset = 77;
+        var scrollTarget = $('#' + pathSub);
+        var scrollLocation = $(scrollTarget).offset().top - scrollOffset;
+
+        $('html, body').animate({
+            scrollTop: scrollLocation
+        }, 500);
+    }
 });
