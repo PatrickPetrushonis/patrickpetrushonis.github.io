dd31286995a75c2e79d595c0ec5e976363dff07b
Patrick Petrushonis <pwp904@gmail.com>
2017-11-01 12:52:47 -0700

    Replaced trianglify generated heroes with image banners.

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,13 +2,9 @@
 // Bower components
 // =require ../bower_components/jquery/dist/jquery.min.js
 
-// Custom plugins
-// =require ../components/trianglify.min.js
-
 // Custom JavaScript partials
 // =require partials/footer.js
 // =require partials/header.js
 // =require partials/scroll.js
-// =require partials/hero.js
 // =require partials/pre.js
 // =require partials/reveal.js
