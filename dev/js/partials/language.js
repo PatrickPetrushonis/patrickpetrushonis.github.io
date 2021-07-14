690124758d2b4d0fb3b132fcdb909c5a50ab0492
Patrick Petrushonis <pwp904@gmail.com>
2021-07-13 21:02:26 -0700

    Added shuffle to phrases with looping upon reaching last index.

--- dev/js/partials/language.js
+++ dev/js/partials/language.js
@@ -8,16 +8,17 @@ $(document).ready(function() {
     });
 
     $('#language-next').on("click", function() {
-        language.getRandomPhrase();
+        language.getNextPhrase();
     });
 });
 
 function languageObj() {
     this.languageData = {},
     this.phrases = [],
-    this.phraseCurrent = {},
+    this.phraseIndices = [],
     this.phraseCategories = [],
-    this.phraseCategory = "Expressions",
+    this.phraseCategory = "",
+    this.phraseIndex = -1,
     this.phraseId = "language-phrase",
     this.helperId = "language-helper",
     this.toggleId = "language-toggle",
@@ -36,41 +37,65 @@ function languageObj() {
         self.languageToggle = document.getElementById(self.toggleId);
         
         jQuery.getJSON("../app/data/mandarin.json", function(data) {            
-            self.languageData = data;
-            self.getRandomPhrase()
-        });
-    }, 
-    /** 
-     * Retrieve random phrase from language data
-     */
-    this.getRandomPhrase = function() {
-        var self = this;
-        self.phrases = [];
+            self.languageData = data;            
+            self.phrases = [];
+            self.phraseCategories = [];
 
-        for (var key in self.languageData) {
-            self.phraseCategories.push(key);
-        }
-        
-        self.phraseCategories.forEach(function(category) {            
-            self.languageData[category].forEach(function(phrase) {
-                self.phrases.push(phrase);
+            // Store language data keys as categories
+            for (var key in self.languageData) {
+                self.phraseCategories.push(key);
+            }
+            
+            // Store each phrase in each category
+            self.phraseCategories.forEach(function(category) {            
+                self.languageData[category].forEach(function(phrase) {
+                    self.phrases.push(phrase);
+                });
             });
+
+            self.phrases = self.shuffleArray(self.phrases);
+            self.getNextPhrase();
         });
-    
-        if (self.phrases.length > 0) {
-            var phraseIndex = Math.floor(Math.random() * self.phrases.length);
-            self.phraseCurrent = self.phrases[phraseIndex];
-            self.convertLanguage();
+    },
+    /** 
+     * Fisher-Yates (aka Knuth) Shuffle
+     */
+    this.shuffleArray = function(array) {
+        var currentIndex = array.length;
+        var randomIndex; 
+        var temp;
+
+        while (--currentIndex > 0){
+            randomIndex = Math.floor(Math.random()*(currentIndex + 1));
+            temp = array[randomIndex];
+            array[randomIndex] = array[currentIndex];
+            array[currentIndex] = temp;
         }
+
+        return array;
+    }
+    /** 
+     * Retrieve next phrase from randomly sorted phrases
+     */
+    this.getNextPhrase = function() {
+        var self = this;
+        var phrasesCount = self.phrases.length;
+        if (phrasesCount <= 0) { return; }
+
+        self.phraseIndex = (self.phraseIndex < phrasesCount - 1) ? self.phraseIndex + 1 : 0;
+        self.updatePhrase();
     },
     /** 
      * Convert current phrase to current language (english or non-english)
      */
-    this.convertLanguage = function() {  
+    this.updatePhrase = function() { 
         var self = this;
 
-        self.languagePhrase.text = self.isEnglish ? self.phraseCurrent.Label : self.phraseCurrent.Mandarin;
-        self.languageHelper.text = self.isEnglish ? "" : self.phraseCurrent.Pinyin;
+        if (self.phraseIndex < 0) { return; }
+        var phrase = self.phrases[self.phraseIndex];
+
+        self.languagePhrase.text = self.isEnglish ? phrase.Label : phrase.Mandarin;
+        self.languageHelper.text = self.isEnglish ? "" : phrase.Pinyin;
     
         $("#" + self.phraseId).text(self.languagePhrase.text);
         $("#" + self.helperId).text(self.languageHelper.text);
@@ -82,6 +107,6 @@ function languageObj() {
         var self = this;
         self.isEnglish = !self.isEnglish;
         //self.languageToggle.text = self.isEnglish ? "English" : "Mandarin";
-        self.convertLanguage();
+        self.updatePhrase();
     }
 }
